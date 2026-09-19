01bd2fe3c9a4801efdf67bde1f1b492c0f728f55
SandeepElectroFix <SandeepElectroFix@gmail.com>
2026-09-19 18:23:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+window.APP_CONFIG = {
+  appName: "Estimate List",
+  businessName: "Sandeep ElectroFix",
+  defaultLanguage: "hi",
+  storageKey: "sandeepEstimateItems",
+  languageKey: "sandeepMaterialLang",
+  themeKey: "sandeepTheme",
+  viewKey: "sandeepMaterialView",
+  ui: {
+    splash: true,
+    menu: true,
+    languageButton: true,
+    bottomNav: true,
+    estimate: true,
+    calculator: true,
+    settings: true,
+    viewSwitch: true,
+    priceField: true,
+    priceInitiallyHidden: true
+  },
+  rules: {
+    quantityRequired: true,
+    otherFieldsOptional: true,
+    autoSave: true,
+    priceOptional: true,
+    skipBrandOption: false
+  }
+};
---
+++ material.js
@@ -0,0 +1,134 @@
+window.MATERIALS = [
+/* STAGE 1 */
+["STAGE 1","Slab Conduit Installation","Conduit & Box",[
+["Pipe",[["Size",["20mm","25mm","32mm","40mm"]],["Type",["Heavy (HMS)","Medium (MMS)","Light (LMS)"]]],["pcs","bndl","doz"],["Polycab","Finolex","Havells","AKG","Precision","Other Brand","Non Brand / Local"]],
+["Bend",[["Size",["20mm","25mm","32mm","40mm"]],["Type",["Heavy","Medium","Light"]],["Sub Type",["Short Bend","Long Bend"]]],["pcs","pkt","doz"],["Polycab","Finolex","Havells","AKG","Precision","Other Brand","Non Brand / Local"]],
+["Junction Box",[["Conduit Size",["20mm","25mm"]],["Type",["Normal Junction Box","Deep Junction Box"]],["Shape / Ways",["1 Way","2 Way Straight","2 Way Angle","3 Way T-Type","4 Way Cross Type","Y-Type","H-Type","U-Type","V-Type"]],["Material",["PVC","GI Metal"]]],["pcs","pkt","doz"],["Polycab","Finolex","AKG","Precision","Local / Brand Master"]],
+["Fan Box",[["Conduit Size",["20mm","25mm"]],["Depth",["2.5″","3″"]],["Material",["MS Metal","PVC"]],["Ways",["4 Way","6 Way","8 Way"]],["Hook Rod",["8mm","10mm"]]],["pcs","pkt","doz"],["Brand Master","Local"]],
+["Concealed Light Box",[["Diameter",["3″","4″"]],["Depth",["3″","3.5″"]],["Material Type",["Heavy GI Metal Box","Heavy PVC Box"]]],["pcs","pkt","doz"],["Brand Master","Local"]]
+],[
+["Installation Material",[
+["Tape (Shuttering & Joint Sealing)",[["Size (Width)",["1″ (24mm)","1.5″ (36mm)","2″ (48mm)","3″ (72mm)"]],["Type",["PVC Joint Tape","BOPP / Shuttering Tape (Brown / Clear)","Duct Tape"]]],["pcs","roll","pkt"],["Steelgrip","Wonder","Anchor","Local / Non-Brand"]],
+["Solvent Cement",[["Pack Size",["50ml","100ml","250ml","500ml","1L"]]],["pcs","tin","can"],["Polycab","Finolex","AKG","Astral","Local"]],
+["Neel Powder (Marking Powder)",[["Pack Size",["100g","250g","500g","1kg"]]],["gm","kg","bag","pkt"],["Standard / Local"]],
+["Binding Wire",[["Gauge / Size",["18 SWG","20 SWG","22 SWG"]]],["kg","mtr","roll"],["Tata","Commercial / Local"]],
+["Cable Tie / Zip Tie",[["Size (Length)",["100mm (4″)","150mm (6″)","200mm (8″)","250mm (10″)","300mm (12″)","400mm (16″)"]],["Colour",["White","Black"]]],["pcs","pkt (100 pcs/pkt)"],["Wonder","3M","Local / Non-Brand"]]
+]]
+],
+],
+/* STAGE 2 */
+["STAGE 2","Wall Conduit Installation","Conduit & Box",[
+["Modular Board (Concealed Metal/PVC Box)",[["Module",["2M","3M","4M","6M","8M Square","8M Rectangular","12M","16M","18M"]],["Type",["GI Metal (18/20 Gauge)","PVC"]]],["Nos","pcs"],["Goldmedal","Anchor","Brand Master","Local"]],
+["Pipe",[["Size",["20mm","25mm","32mm","40mm"]],["Type",["Heavy (HMS)","Medium (MMS)","Light (LMS)"]]],["pcs","bndl","doz"],["Polycab","Finolex","Havells","AKG","Precision","Other Brand","Non Brand / Local"]],
+["Bend",[["Size",["20mm","25mm","32mm","40mm"]],["Type",["Heavy","Medium","Light"]],["Sub Type",["Short Bend","Long Bend"]]],["pcs","pkt","doz"],["Polycab","Finolex","Havells","AKG","Precision","Other Brand","Non Brand / Local"]],
+["Junction Box",[["Conduit Size",["20mm","25mm"]],["Type",["Normal Junction Box","Deep Junction Box"]],["Shape / Ways",["1 Way","2 Way Straight","2 Way Angle","3 Way T-Type","4 Way Cross Type","Y-Type","H-Type","U-Type","V-Type"]],["Material",["PVC","GI Metal"]]],["pcs","pkt","doz"],["Polycab","Finolex","AKG","Precision","Local / Brand Master"]],
+["MCB Box (Distribution Board)",[["Size",["2 Way","4 Way","6 Way","8 Way","10 Way","12 Way","16 Way","18 Way","24 Way"]],["Phase Selection",["Single Phase (SPN)","Three Phase (TPN)"]],["Door Type",["Single Door","Double Door","Transparent (Acrylic) Door"]],["Material Type",["Metal / Sheet Steel (CRCA Powder Coated)","PVC"]]],["pcs"],["Havells","Legrand","Schneider","L&T","Anchor","Local"]]
+],[
+["Installation Material",[
+["Tape (Masking & Plaster Protection)",[["Size (Width)",["1/2″ (12mm)","1″ (24mm)","2″ (48mm)"]],["Type",["Masking / Paper Tape","PVC Insulation Tape"]]],["pcs","roll","pkt"],["Wonder","Steelgrip","Anchor","Local / Non-Brand"]],
+["Cable Clip",[["Size",["4mm","6mm","8mm","10mm","12mm","14mm","16mm","20mm","25mm","32mm"]],["Type",["Round Clip (Steel Nail)","Flat Clip (Steel Nail)"]]],["pcs","pkt (100 pcs/pkt)"],["Standard / Local"]]
+]]
+],
+],
+/* STAGE 3 */
+["STAGE 3","Wiring Installation","Wiring Material",[
+["Wire",[["Size",["0.75 Sqmm","1 Sqmm","1.5 Sqmm","2.5 Sqmm","4 Sqmm","6 Sqmm","10 Sqmm"]],["Type",["FR (Flame Retardant)","HRFR (Heat Resistant)","FRLS (Low Smoke)","XLPE"]],["Colour",["Red","Black","Yellow","Blue","Green","White","Grey"]]],["mtr","bndl (90m / 180m roll)"],["Polycab","Finolex","Havells","RR Kabel","KEI","Anchor","Local"]],
+["Flexible Pipe",[["Size",["16mm","20mm","25mm","32mm"]],["Type",["PVC Flexible Pipe","FMC (GI / Steel Metallic)","LFMC (PVC Coated Liquid-tight)"]]],["mtr","bndl (15m / 25m / 50m)"],["AKG","Precision","National","Local"]],
+["Electrical Tape",[["Size (Width × Length)",["18mm × 7m","18mm × 8m","18mm × 10m"]],["Colour",["Red","Black","Yellow","Blue","Green","White","Grey"]]],["pcs","bx","doz"],["Steelgrip","3M","Wonder","Anchor","Local"]],
+["Fastener",[["Size",["M4","M5","M6","M8","M10","M12","M16","M20"]],["Type",["Wedge Anchor Fastener","Pin Type Anchor","Rawl Anchor Bolt","Drop In Anchor","J Hook Anchor","Round Anchor Bolt"]]],["pcs","bx","doz"],["Hilti","Fischer","Standard / Local"]]
+],[
+["Pulling Material",[
+["Steel Wire / Spring Wire (Fish Tape)",[["Size / Diameter",["16 SWG (1.6mm)","18 SWG (1.2mm)","20 SWG (0.9mm)","3mm","4mm","6mm"]],["Type",["GI Steel Pulling Wire","Flat Spring Steel Tape","Nylon / Fiberglass Puller"]],["Length",["10 mtr","15 mtr","20 mtr","30 mtr","50 mtr"]]],["kg","mtr","pcs / roll"],["Tata (GI)","Commercial / Local"]]
+]]
+],
+],
+/* STAGE 4 */
+["STAGE 4","Final Electrical Fittings","Switch & Socket",[
+["Switch Plate",[["Module",["1M","2M","3M","4M","6M","8M Horizontal","8M Square / Vertical","12M","16M","18M"]],["Type",["PVC Modular Plate","Acrylic / Mica Plate","Glass Plate","Metal Finish Plate"]]],["pcs"],["Legrand","Schneider","Anchor (Roma)","GM","Havells","Crabtree"]],
+["Switch Board (Surface Gang Box)",[["Module",["1M","2M","3M","4M","6M","8M","12M","16M","18M"]],["Type",["PVC Surface Gang Box","Wooden Board"]]],["pcs"],["Anchor","GM","Cona","Local"]],
+["Switch",[["Amp",["6A","10A","16A","20A","25A"]],["Type",["1 Way Switch","Intermediate Switch"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor (Roma)","GM","Havells","Crabtree"]],
+["Socket",[["Amp",["5A","6A","10A","16A","20A","25A"]],["Type",["2 Pin Socket","3 Pin Multi Socket","6/16A Combined Heavy Socket","10/25A Heavy Socket"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor (Roma)","GM","Havells","Crabtree"]],
+["Fan Regulator",[["Module",["1M","2M"]],["Type",["Step Regulator (4/5 Steps)","Electronic / Stepless"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor (Roma)","GM","Havells","Crabtree"]],
+["2 Way Switch",[["Amp",["6A","10A","16A","20A"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor (Roma)","GM","Havells","Crabtree"]],
+["Bell Push",[["Amp",["6A","10A"]],["Type",["Standard Bell Push","Bell Push with Indicator / Nameplate"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor","GM","Havells"]],
+["Neon Indicator",[["Colour",["Red","Green","Yellow","Blue","White"]],["Type",["LED Type","Neon Tube Type (1M Modular)"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor","GM","Havells"]],
+["Blank Plate / Dummy Switch",[["Type",["Modular Blank Insert","Dummy Plate"]],["Module",["1M","2M"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor","GM","Havells"]],
+["DP Switch (Double Pole Switch)",[["Amp",["16A","20A","25A","32A","40A","63A"]]],["pcs","bx","doz"],["Legrand","Schneider","Anchor","GM","Havells"]]
+],[
+["MCB & Protection",[
+["Mini MCB",[["Amp",["6A","10A","16A","20A","25A","32A"]],["Curve",["C Curve"]],["Module",["1M","2M"]]],["pcs","bx"],["Legrand","Schneider","Havells","GM","Anchor"]],
+["SP MCB (Single Pole)",[["Amp",["0.5A","1A","1.6A","2A","3A","4A","6A","8A","10A","13A","16A","20A","25A","32A","40A","50A","63A"]],["Curve",["B Curve","C Curve","D Curve"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","Siemens","ABB"]],
+["DP MCB (Double Pole)",[["Amp",["6A","10A","16A","20A","25A","32A","40A","50A","63A"]],["Curve",["B Curve","C Curve","D Curve"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","Siemens","ABB"]],
+["TPN MCB (Three Pole with Neutral)",[["Amp",["16A","20A","25A","32A","40A","50A","63A"]],["Curve",["B Curve","C Curve","D Curve"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","Siemens","ABB"]],
+["MCB Changeover",[["Amp",["16A","25A","32A","40A","63A","80A","100A","125A"]],["Type",["Manual (DP / 4P MCB Type)","Automatic (ATS)"]]],["pcs","bx"],["Havells","Legrand","L&T","Schneider","HPL"]],
+["DP Isolator",[["Amp",["16A","20A","25A","32A","40A","63A","80A","100A","125A"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","ABB"]],
+["TPN Isolator (3P / 4P)",[["Amp",["16A","20A","25A","32A","40A","63A","80A","100A","125A"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","ABB"]],
+["RCCB / RCD",[["Amp",["25A","40A","63A","80A","100A"]],["Sensitivity",["30mA","100mA","300mA"]],["Type",["Type AC","Type A"]]],["pcs","bx"],["Legrand","Schneider","Havells","L&T","Siemens"]],
+["MCB Box",[["Size",["2 Way","4 Way","6 Way","8 Way","10 Way","12 Way","16 Way","18 Way","24 Way"]],["Door",["Single Door","Double Door","Transparent (Acrylic) Door"]],["Material",["Sheet Steel (CRCA)","PVC"]]],["pcs"],["Havells","Legrand","Schneider","L&T","Standard / Local"]],
+["Kit Kat Fuse",[["Amp",["5A","10A","15A","20A","25A","30A","40A","50A","60A","80A","100A"]],["Voltage",["250V (Single Phase)","415V (Three Phase)"]]],["pcs"],["Anchor","Havells","Commercial / Local"]]
+]],
+["Fan & Ceiling",[
+["Fan Sheet",[["Type",["PVC Round Plate","Acrylic / Mica Plate"]]],["pcs","pkt","doz"],["Standard / Local"]],
+["Round Sheet",[["Type",["PVC Junction Cover Sheet","Mica Sheet"]]],["pcs","pkt","doz"],["Standard / Local"]],
+["Fan Rod",[["Size (Length)",["9″","12″","15″","18″","24″","30″","36″","42″","48″"]]],["pcs"],["Orient","Crompton","Havells","Heavy GI Local"]],
+["Fan Clamp",[["Type",["Heavy MS Clamp","Normal Z-Clamp / U-Clamp"]]],["pcs"],["Standard / Local"]],
+["Holder",[["Type",["Batten Holder (Straight)","Angle Holder"]],["Material",["Polycarbonate with Brass / Ceramic Thread"]]],["pcs","bx","doz"],["Anchor","GM","Cona","Havells","Local"]],
+["Ceiling Rose",[["Type",["2 Plate Ceiling Rose","3 Plate Ceiling Rose"]]],["pcs","bx","doz"],["Anchor","GM","Cona","Havells","Local"]],
+["Chain",[["Length",["User Input (ft / inch)"]],["Material",["GI Steel Link","Decorative Brass / Antique Finish"]]],["inch","ft"],["Standard Metal / Local"]]
+]],
+["Lighting",[
+["LED Bulb",[["Wattage",["3W","5W","7W","9W","12W","15W","20W"]],["Base",["B22 (Pin Type)","E27 (Screw Type)"]],["Colour Temp",["Warm White 3000K","Neutral 4000K","Cool Daylight 6500K"]]],["pcs"],["Philips","Havells","Syska","Wipro","Crompton","Halonix"]],
+["LED Tube Light",[["Length",["2 ft","4 ft"]],["Wattage",["10W","20W","40W","50W"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs","bx"],["Philips","Havells","Syska","Wipro","Crompton"]],
+["Foot Light",[["Wattage",["3W","6W","12W"]],["Mounting",["Recessed / Concealed","Surface Mount"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["GM","Legrand","Philips","Havells","Local"]],
+["Up Down Light",[["Wattage",["3W × 2 (6W)","5W × 2 (10W)","10W","12W"]],["Type",["Indoor Wall Light","Outdoor Waterproof IP65"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["Philips","Havells","Jaquar","Local Architectural"]],
+["Panel Light",[["Wattage",["6W","8W","12W","15W","18W","22W","24W","36W"]],["Mounting",["Recessed / Concealed","Surface Mount"]],["Shape",["Round","Square"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["Philips","Havells","Wipro","Syska","Crompton"]],
+["Surface Light",[["Wattage",["6W","12W","15W","18W","22W","24W"]],["Shape",["Round","Square"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["Philips","Havells","Wipro","Syska","Crompton"]],
+["COB Light",[["Wattage",["3W","5W","7W","10W","12W","15W","18W","20W","30W"]],["Type",["Recessed / Concealed","Surface","Anti-Glare"]],["Shape",["Round","Square"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["Philips","Havells","Jaquar","Luker","Local"]],
+["COB Spot Light",[["Wattage",["2W","3W","5W","7W","10W","12W","15W"]],["Mounting",["Deep Recessed / Anti-Glare","Surface Mount"]],["Movement",["Fixed","Swivel / Gimbal (Tilting)"]],["Beam Angle",["Narrow (15°–24°)","Wide (36°–60°)"]],["Colour Temp",["3000K","4000K","6500K"]],["Body Finish",["Full White","White + Black Ring","Full Black","Rose Gold","Chrome"]]],["pcs","bx"],["Philips","Havells","Jaquar","Local Architectural"]],
+["Down Light",[["Wattage",["3W","5W","7W","10W","12W","15W","18W","22W","24W"]],["Type",["Recessed / Concealed","Surface"]],["Shape",["Round","Square"]],["Colour Temp",["3000K","4000K","6500K"]]],["pcs"],["Philips","Havells","Wipro","Syska","Crompton"]],
+["Strip Light",[["Density",["60 LED/m","120 LED/m","240 LED/m"]],["Voltage",["12V DC","24V DC"]],["Length",["5m roll","10m roll"]],["Colour",["Warm White","Cool White","Natural White","Red","Blue","Green","Pink","RGB"]]],["roll"],["Philips","Havells","GM","Wipro","Commercial"]],
+["Rope Light",[["Supply Voltage",["AC 220V","DC 12V","DC 24V"]],["Colour",["Warm White","Cool White","Red","Green","Blue","Yellow/Amber","Pink","Purple","Ice Blue","Orange","RGB"]],["Length",["User Input (Meters)"]]],["mtr","roll"],["Philips","Havells","Syska","Commercial / Local"]],
+["LED Profile Channel",[["Dimensions (W × D)",["17×8 mm","17×14 mm","24×10 mm","35×20 mm","16×16 mm"]],["Mounting",["Concealed (Flanged/Winged)","Surface Mount","Corner (V-Shape)","Hanging"]],["Length",["1 mtr","2 mtr","3 mtr"]],["Diffuser",["Milky White","Smoked Black"]]],["pcs","mtr"],["Stanjo","Philips","Architectural Aluminum","Local"]],
+["LED Strip Driver (SMPS)",[["Wattage",["36W (3A)","60W (5A)","120W (10A)","150W (12.5A)","200W (16.6A)","250W (20A)","300W (25A)","400W (33A)"]],["Output Voltage",["12V DC","24V DC"]],["Type",["Ultra-Slim Profile Driver","Metal Mesh SMPS","Rainproof Outdoor (IP67)","Plastic Adapter"]]],["pcs"],["Mean Well","Philips","GM","Havells","Standard Electronic"]]
+]],
+["Installation & Finishing",[
+["Door Bell",[["Type",["Ding Dong Mechanical","Polyphonic / Multi-Tune Musical","Wireless Remote"]],["Voltage",["220V AC","Battery Operated (DC)"]]],["pcs"],["Anchor","GM","Cona","Havells","Goldmedal"]],
+["Electrical Tape",[["Size (Width × Length)",["18mm × 7m","18mm × 8m","18mm × 10m"]],["Colour",["Red","Black","Yellow","Blue","Green","White","Grey"]]],["pcs"],["Steelgrip","3M","Wonder","Anchor","Local"]],
+["Tape (Mounting / Double Sided)",[["Size (Width)",["1/2″","1″","2″"]],["Type",["Double Sided Acrylic Foam Tape","Paper Masking Tape"]]],["pcs","roll"],["3M","Wonder","Local"]],
+["Instant Glue",[["Size (Weight)",["0.5g","1g","2g","3g","20g"]]],["pcs"],["Fevikwik","Quick Fix","Local"]],
+["Araldite Glue (Epoxy)",[["Pack Size",["10g","36g","90g","180g"]],["Type",["Standard (Slow Setting)","Fast & Clear (Quick Setting)"]]],["pcs","pkt"],["Araldite (Huntsman)"]],
+["POP (Plaster of Paris)",[["Pack Size (Weight)",["1kg","2kg","5kg","20kg","40kg"]]],["kg","bag","pkt"],["Sakarni","JK Lakshmi","Birla White","Local"]],
+["Putty Blade / Patta",[["Size",["2″","3″","4″","5″","6″"]]],["pcs"],["Standard Steel / Local"]],
+["Fastener",[["Size",["M4","M5","M6","M8","M10","M12","M16","M20"]],["Type",["Wedge Anchor Fastener","Pin Type Anchor","Rawl Anchor Bolt","Drop In Anchor","J Hook Anchor"]]],["pcs","bx","doz"],["Hilti","Fischer","Local MS / GI"]],
+["Screw",[["Size (Diameter × Length)",["2.2×6.5mm","2.9×8mm","3.5×13mm","3.9×19mm","4.2×25mm","4.8×32mm","5.5×50mm","6.3×75mm"]],["Type",["Self Tapping","Self Drilling (Pan / CSK Head)"]]],["pkt (100 pcs/pkt)","box"],["Standard GI / Hardened Steel"]],
+["Lug (Cable Terminal Lug)",[["Size (Cable Size × Stud Size)",["1.5×M4","1.5×M5","2.5×M4","2.5×M5","4×M6","6×M6","10×M6","10×M8","16×M8","25×M10","35×M10","50×M12","70×M12","95×M12","120×M12","150×M12","185×M12","240×M12"]],["Type",["Ring Type","Pin Type","Fork / Spade Type","Butt Connector"]],["Material",["Copper (CU)","Aluminium (ALU)","Bimetallic (AL-CU)"]]],["pcs","pkt","box"],["Dowells","Comet","Brand Master","Local"]],
+["Washer",[["Size",["M4","M5","M6","M8","M10","M12","M16","M20"]],["Type",["Plain Flat Washer","Spring Washer","Star / Internal Tooth Washer"]],["Material",["Mild Steel (MS)","Galvanized Iron (GI)","Stainless Steel (SS 304)","Brass"]]],["pcs","pkt","gm","kg"],["Standard / Local"]]
+]]
+],
+],
+/* STAGE 5 */
+["STAGE 5","False Ceiling Wiring Material","Wiring & Conduit",[
+["Wire",[["Size",["0.75 Sqmm","1 Sqmm","1.5 Sqmm","2.5 Sqmm","4 Sqmm"]],["Type",["FR","FRLS"]],["Colour",["Red","Black","Yellow","Blue","Green","White"]]],["mtr","bndl"],["Polycab","Finolex","Havells","RR Kabel","KEI","Local"]],
+["Electrical Tape",[["Size (Width × Length)",["18mm × 7m","18mm × 8m","18mm × 10m"]],["Colour",["Red","Black","Yellow","Blue","Green","White","Grey"]]],["pcs","bx"],["Steelgrip","3M","Wonder","Local"]],
+["Flexible Pipe",[["Size",["16mm","20mm","25mm","32mm"]],["Type",["PVC Flexible","FMC (GI Steel Metallic)"]]],["mtr","bndl"],["AKG","Precision","National","Local"]],
+["Pipe",[["Size",["20mm","25mm"]],["Type",["Medium (MMS)","Light (LMS)","Heavy (HMS)"]]],["pcs","bndl"],["Polycab","Finolex","AKG","Precision","Local"]],
+["Bend",[["Size",["20mm","25mm"]],["Type",["Medium","Light"]],["Sub Type",["Short Bend","Long Bend"]]],["pcs","pkt"],["Polycab","Finolex","AKG","Precision","Local"]],
+["Junction Box",[["Conduit Size",["20mm","25mm"]],["Type",["Normal Junction Box","Deep Junction Box"]],["Shape / Ways",["1 Way","2 Way Straight","2 Way Angle","3 Way T-Type","4 Way Cross Type"]],["Material",["PVC","GI Metal"]]],["pcs","pkt"],["Polycab","Finolex","AKG","Precision","Local"]]
+],[
+["Installation & Fastening",[
+["Cable Tie / Zip Tie",[["Size (Length)",["100mm (4″)","150mm (6″)","200mm (8″)","250mm (10″)","300mm (12″)"]],["Colour",["White","Black"]]],["pcs","pkt (100 pcs/pkt)"],["Wonder","3M","Local"]],
+["Cable Clip",[["Size",["4mm","6mm","8mm","10mm","12mm","14mm","16mm","20mm"]],["Type",["Round Clip with Nail","Flat Clip with Nail"]]],["pcs","pkt"],["Standard / Local"]],
+["Saddle (Pipe Clamp)",[["Size",["20mm","25mm","32mm"]],["Type",["Half Saddle (Open)","Full Saddle (with Base)"]],["Material",["GI Steel","PVC"]]],["pcs","pkt"],["Standard / Local"]],
+["Fastener",[["Size",["M6","M8","M10"]],["Type",["Drop In Anchor (Bullet Fastener)","Pin Type Anchor","Wedge Anchor"]]],["pcs","bx"],["Hilti","Fischer","Local"]],
+["PVC Wall Plug / Gulli / Gitti",[["Size",["No. 4","No. 5","No. 6","No. 8","No. 10 (30mm, 35mm, 40mm)"]],["Type",["Standard Ribbed PVC Plug"]]],["pkt","bx"],["Standard / Local"]],
+["Screw",[["Size (Diameter × Length)",["3.5×19mm","3.5×25mm","3.5×32mm","3.5×38mm","4.2×25mm","4.2×38mm","4.2×50mm"]],["Type",["Drywall / Gypsum Screw (Black)","Self Drilling (Pan / Star Head)"]]],["pkt (100 pcs/pkt)","box"],["Standard / Local"]],
+["Washer",[["Size",["M6","M8","M10"]],["Type",["Plain Flat Washer","Spring Washer"]],["Material",["Mild Steel (MS)","Galvanized Iron (GI)","Stainless Steel (SS)"]]],["pcs","pkt","gm","kg"],["Standard / Local"]]
+]],
+["Fan & Ceiling",[
+["Fan Rod",[["Size (Length)",["9″","12″","15″","18″","24″","30″","36″","42″","48″"]]],["pcs"],["Orient","Crompton","Havells","Heavy GI Local"]],
+["Fan Clamp",[["Type",["Heavy Duty Z-Clamp","U-Clamp","Threaded Rod Anchor Clamp"]]],["pcs"],["Standard / Local"]],
+["Chain",[["Length",["User Input (ft / inch)"]],["Type",["Heavy GI Welded Link","Decorative Link Chain"]]],["ft","inch"],["Standard / Local"]]
+]]
+]
+]
+];
